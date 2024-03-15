18f813367c2745b2a45dc6b8fd9e69bc0f7f9588
ogNG4 <m.podobinski33@gmail.com>
2024-03-15 21:05:59 +0100

    feat(AP-12): initial

---
+++ mkmodel.py
@@ -0,0 +1,14 @@
+import pathlib
+
+import spacy
+
+if __name__ == "__main__":
+    nlp = spacy.load("en_core_web_sm")
+    rules_file = 'matcher-rules/proglang.jsonl'
+    print(f"Will now create model for {rules_file}.")
+    ruler = nlp.add_pipe('entity_ruler')
+    ruler.from_disk(rules_file)
+
+    nlp.meta["name"] = "proglang"
+    nlp.to_disk(nlp.meta["name"])
+    print(f"spaCy model saved over at {nlp.meta['name']}.")
